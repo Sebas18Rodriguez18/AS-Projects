dcd4a1c71efbbfe38d8ac2d2a799e4b2b13144e6
Juan Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-12-18 20:32:38 -0500

    Block
    Implementación de Game Loop con Handler y Runnable para caída automática

--- app/src/main/java/com/example/mytetris/MainActivity.kt
+++ app/src/main/java/com/example/mytetris/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.example.mytetris
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var piezaActual: View? = null
+    private var filaActual = 0
+    private val handler = Handler(Looper.getMainLooper())
+
+    private var tamanoPixel = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val densidad = resources.displayMetrics.density
+        tamanoPixel = (30 * densidad).toInt()
+
+        crearPrimerBloque()
+    }
+
+    private fun crearPrimerBloque() {
+        val gameBoard = findViewById<FrameLayout>(R.id.game_board)
+
+        val bloque = View(this)
+
+        val params = FrameLayout.LayoutParams(tamanoPixel, tamanoPixel)
+        params.leftMargin = tamanoPixel * 4
+        params.topMargin = tamanoPixel * 0
+
+        bloque.setBackgroundResource(R.drawable.bloque_base)
+        bloque.layoutParams = params
+
+        gameBoard.addView(bloque)
+
+        piezaActual = bloque
+        filaActual = 0
+    }
+
+    private val caerRunnable = object : Runnable {
+        override fun run() {
+            filaActual++
+
+            val params = piezaActual?.layoutParams as? FrameLayout.LayoutParams
+            if (params != null) {
+                params.topMargin = filaActual * tamanoPixel
+                piezaActual?.layoutParams = params
+            }
+
+            handler.postDelayed(this, 500)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(caerRunnable)
     }
 }
